6d2f723c8758d80ff7b1095818e2191846bb0f98
Hangyu Sang / 상한규 <68945250+LittleWaterDrops@users.noreply.github.com>
2020-08-21 10:44:23 +0900

    add velocity

--- 소스.c
+++ 소스.c
@@ -5,31 +5,36 @@
 
 #if 1
 int main(void) {
-	char readFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\3\\3.csv";
-	char writeFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\coordinateTransformedData\\Transed_3.csv";
-	char timeFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\3\\time.txt";
-	char xposFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\3\\xpos.txt";
-	char yposFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\3\\ypos.txt";
-	char zposFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\3\\zpos.txt";
+	char readFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\10\\10.csv";
+	char writeFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\coordinateTransformedData\\Transed_10.csv";
+	char timeFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\10\\time.txt";
+	char velocityFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\10\\velocity.txt";
+	char xposFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\10\\xpos.txt";
+	char yposFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\10\\ypos.txt";
+	char zposFileName[200] = "C:\\Users\\nic6\\Desktop\\coordinate_transform\\realRun\\10\\zpos.txt";
 	char* bufferToCountCell;
 	char* bufferToPushData;
 	char *bufferToCountTime;
+	char* bufferToCountVelocity;
 	char* bufferToCountXpos;
 	char* bufferToCountYpos;
 	char* bufferToCountZpos;
 	char buffer[10];
 	int readFileSize;
 	int timeSize;
+	int velocitySize;
 	int xposSize;
 	int yposSize;
 	int zposSize;
 	int readCellCount = 0;
 	int timeCount = 0;
+	int velocityCount = 0;
 	int xposCount = 0;
 	int yposCount = 0;
 	int zposCount = 0;
 	int trash = 0;
 	float timeArray[25000] = { 0, };
+	float velocityArray[25000] = { 0, };
 	float xposArray[25000] = { 0, };
 	float yposArray[25000] = { 0, };
 	float zposArray[25000] = { 0, };
@@ -37,6 +42,7 @@ int main(void) {
 	FILE* readFile = NULL;
 	FILE* writeFile = NULL;
 	FILE* timeFile = NULL;
+	FILE* velocityFile = NULL;
 	FILE* xposFile = NULL;
 	FILE* yposFile = NULL;
 	FILE* zposFile = NULL;
@@ -44,6 +50,7 @@ int main(void) {
 	readFile = fopen(readFileName, "r");
 	writeFile = fopen(writeFileName, "w");
 	timeFile = fopen(timeFileName, "r");
+	velocityFile = fopen(velocityFileName, "r");
 	xposFile = fopen(xposFileName, "r");
 	yposFile = fopen(yposFileName, "r");
 	zposFile = fopen(zposFileName, "r");
@@ -58,19 +65,21 @@ int main(void) {
 	else 
 		printf("쓸 파일을 열었습니다.\n");
 	
-	if (timeFile == NULL || xposFile == NULL|| yposFile == NULL|| zposFile == NULL) 
-		printf("좌표 및 시간 파일을 열 수 없습니다.\n");
+	if (timeFile == NULL || velocityFile == NULL || xposFile == NULL|| yposFile == NULL|| zposFile == NULL) 
+		printf("좌표 및 속도 혹은 시간 파일을 열 수 없습니다.\n");
 	else 
-		printf("좌표 및 시간 파일을 열었습니다.\n");
+		printf("좌표 및 속도 혹은 시간 파일을 열었습니다.\n");
 	
 	fseek(readFile, 0, SEEK_END);		//파일 포인터를 파일 끝으로 이동시킴
 	fseek(timeFile, 0, SEEK_END);    // 파일 포인터를 파일의 끝으로 이동시킴
+	fseek(velocityFile, 0, SEEK_END);    // 파일 포인터를 파일의 끝으로 이동시킴
 	fseek(xposFile, 0, SEEK_END);    // 파일 포인터를 파일의 끝으로 이동시킴
 	fseek(yposFile, 0, SEEK_END);    // 파일 포인터를 파일의 끝으로 이동시킴
 	fseek(zposFile, 0, SEEK_END);    // 파일 포인터를 파일의 끝으로 이동시킴
 
 	readFileSize = ftell(readFile);			// 파일 포인터의 현재 위치를 얻음
 	timeSize = ftell(timeFile);          // 파일 포인터의 현재 위치를 얻음
+	velocitySize = ftell(velocityFile);          // 파일 포인터의 현재 위치를 얻음
 	xposSize = ftell(xposFile);          // 파일 포인터의 현재 위치를 얻음
 	yposSize = ftell(yposFile);          // 파일 포인터의 현재 위치를 얻음
 	zposSize = ftell(zposFile);          // 파일 포인터의 현재 위치를 얻음
@@ -81,6 +90,8 @@ int main(void) {
 	memset(bufferToPushData, 0, readFileSize + 1);  // 파일 크기 + 1바이트만큼 메모리를 0으로 초기화
 	bufferToCountTime = malloc(timeSize + 1);    // 파일 크기 + 1바이트(문자열 마지막의 NULL)만큼 동적 메모리 할당
 	memset(bufferToCountTime, 0, timeSize + 1);  // 파일 크기 + 1바이트만큼 메모리를 0으로 초기화
+	bufferToCountVelocity = malloc(velocitySize + 1);    // 파일 크기 + 1바이트(문자열 마지막의 NULL)만큼 동적 메모리 할당
+	memset(bufferToCountVelocity, 0, velocitySize + 1);  // 파일 크기 + 1바이트만큼 메모리를 0으로 초기화
 	bufferToCountXpos = malloc(xposSize + 1);    // 파일 크기 + 1바이트(문자열 마지막의 NULL)만큼 동적 메모리 할당
 	memset(bufferToCountXpos, 0, xposSize + 1);  // 파일 크기 + 1바이트만큼 메모리를 0으로 초기화
 	bufferToCountYpos = malloc(yposSize + 1);    // 파일 크기 + 1바이트(문자열 마지막의 NULL)만큼 동적 메모리 할당
@@ -92,6 +103,8 @@ int main(void) {
 	fread(bufferToCountCell, readFileSize, 1, readFile);    // csv 파일 크기만큼 값을 읽음
 	fseek(timeFile, 0, SEEK_SET);                // 파일 포인터를 파일의 처음으로 이동시킴
 	fread(bufferToCountTime, timeSize, 1, timeFile);    // csv 파일 크기만큼 값을 읽음
+	fseek(velocityFile, 0, SEEK_SET);                // 파일 포인터를 파일의 처음으로 이동시킴
+	fread(bufferToCountVelocity, velocitySize, 1, velocityFile);    // csv 파일 크기만큼 값을 읽음
 	fseek(xposFile, 0, SEEK_SET);                // 파일 포인터를 파일의 처음으로 이동시킴
 	fread(bufferToCountXpos, xposSize, 1, xposFile);    // csv 파일 크기만큼 값을 읽음
 	fseek(yposFile, 0, SEEK_SET);                // 파일 포인터를 파일의 처음으로 이동시킴
@@ -106,6 +119,15 @@ int main(void) {
 		timeArray[timeCount] = atof(timePtr);
 		timeCount++;
 		timePtr = strtok(NULL, "\n");      // 다음 문자열을 잘라서 포인터를 반환
+	}
+
+	char* velocityPtr = strtok(bufferToCountVelocity, "\n");      // " " 를 기준으로 문자열을 자름, 포인터 반환
+
+	while (velocityPtr != NULL)               // 자른 문자열이 나오지 않을 때까지 반복
+	{
+		velocityArray[velocityCount] = atof(velocityPtr);
+		velocityCount++;
+		velocityPtr = strtok(NULL, "\n");      // 다음 문자열을 잘라서 포인터를 반환
 	}
 
 	char* XposPtr = strtok(bufferToCountXpos, "\n");      // " "를 기준으로 문자열을 자름, 포인터 반환
@@ -165,7 +187,7 @@ int main(void) {
 		ptr2 = strtok(NULL, ",");      // 다음 문자열을 잘라서 포인터를 반환
 	}
 	
-	fputs("Position X,Position Y,Position Z,AccX,AccY,AccZ,GyroX,GyroY,GyroZ,MagX,MagY,MagZ,Minor201,Minor202,Minor203,Minor204,Minor205,Minor206,Minor207,Minor208,Minor209,Minor210,Minor211,Minor212,Minor213,Minor214,Minor215,Minor216,Minor217,Minor218,Minor219,Minor220,Minor221,Minor222,Minor223,Minor224,Minor225,Minor226,Minor227,Minor228,Minor229,Minor230,Minor231,Minor232,Minor233,Minor234,Minor235,Minor236,Minor237,Minor238,Minor239,Minor240,Minor241,Minor242,Minor243,Minor244,Minor245,Minor246,Minor247,Minor248,Minor249,Minor250,Minor251,Minor252,Minor253,Minor254,Minor255,Minor256,Minor257,Minor258,Minor259,Minor260\n", writeFile);
+	fputs("Position X,Position Y,Position Z,Velocity,AccX,AccY,AccZ,GyroX,GyroY,GyroZ,MagX,MagY,MagZ,Minor201,Minor202,Minor203,Minor204,Minor205,Minor206,Minor207,Minor208,Minor209,Minor210,Minor211,Minor212,Minor213,Minor214,Minor215,Minor216,Minor217,Minor218,Minor219,Minor220,Minor221,Minor222,Minor223,Minor224,Minor225,Minor226,Minor227,Minor228,Minor229,Minor230,Minor231,Minor232,Minor233,Minor234,Minor235,Minor236,Minor237,Minor238,Minor239,Minor240,Minor241,Minor242,Minor243,Minor244,Minor245,Minor246,Minor247,Minor248,Minor249,Minor250,Minor251,Minor252,Minor253,Minor254,Minor255,Minor256,Minor257,Minor258,Minor259,Minor260\n", writeFile);
 
 	for (int i = 0; i < readCellCount; i++) {
 		char* accInterval = "accInterval\0";
@@ -193,6 +215,10 @@ int main(void) {
 					fputs(buffer, writeFile);
 					memset(buffer, 0, sizeof(buffer));
 					fputs(",", writeFile);
+					sprintf(buffer, "%f", velocityArray[j]);
+					fputs(buffer, writeFile);
+					memset(buffer, 0, sizeof(buffer));
+					fputs(",", writeFile);
 					trash = 0;
 					break;
 				}
@@ -210,6 +236,10 @@ int main(void) {
 						fputs(buffer, writeFile);
 						memset(buffer, 0, sizeof(buffer));
 						fputs(",", writeFile);
+						sprintf(buffer, "%f", velocityArray[j]);
+						fputs(buffer, writeFile);
+						memset(buffer, 0, sizeof(buffer));
+						fputs(",", writeFile);
 						trash = 0;
 						break;
 					}
@@ -223,6 +253,10 @@ int main(void) {
 						memset(buffer, 0, sizeof(buffer));
 						fputs(",", writeFile);
 						sprintf(buffer, "%f", zposArray[j + 1]);
+						fputs(buffer, writeFile);
+						memset(buffer, 0, sizeof(buffer));
+						fputs(",", writeFile);
+						sprintf(buffer, "%f", velocityArray[j + 1]);
 						fputs(buffer, writeFile);
 						memset(buffer, 0, sizeof(buffer));
 						fputs(",", writeFile);
@@ -263,6 +297,10 @@ int main(void) {
 					fputs(buffer, writeFile);
 					memset(buffer, 0, sizeof(buffer));
 					fputs(",", writeFile);
+					sprintf(buffer, "%f", velocityArray[j]);
+					fputs(buffer, writeFile);
+					memset(buffer, 0, sizeof(buffer));
+					fputs(",", writeFile);
 					trash = 0;
 					break;
 				}
@@ -280,6 +318,10 @@ int main(void) {
 						fputs(buffer, writeFile);
 						memset(buffer, 0, sizeof(buffer));
 						fputs(",", writeFile);
+						sprintf(buffer, "%f", velocityArray[j]);
+						fputs(buffer, writeFile);
+						memset(buffer, 0, sizeof(buffer));
+						fputs(",", writeFile);
 						trash = 0;
 						break;
 					}
@@ -293,6 +335,10 @@ int main(void) {
 						memset(buffer, 0, sizeof(buffer));
 						fputs(",", writeFile);
 						sprintf(buffer, "%f", zposArray[j + 1]);
+						fputs(buffer, writeFile);
+						memset(buffer, 0, sizeof(buffer));
+						fputs(",", writeFile);
+						sprintf(buffer, "%f", velocityArray[j + 1]);
 						fputs(buffer, writeFile);
 						memset(buffer, 0, sizeof(buffer));
 						fputs(",", writeFile);
@@ -334,6 +380,10 @@ int main(void) {
 					fputs(buffer, writeFile);
 					memset(buffer, 0, sizeof(buffer));
 					fputs(",", writeFile);
+					sprintf(buffer, "%f", velocityArray[j]);
+					fputs(buffer, writeFile);
+					memset(buffer, 0, sizeof(buffer));
+					fputs(",", writeFile);
 					trash = 0;
 					break;
 				}
@@ -351,6 +401,10 @@ int main(void) {
 						fputs(buffer, writeFile);
 						memset(buffer, 0, sizeof(buffer));
 						fputs(",", writeFile);
+						sprintf(buffer, "%f", velocityArray[j]);
+						fputs(buffer, writeFile);
+						memset(buffer, 0, sizeof(buffer));
+						fputs(",", writeFile);
 						trash = 0;
 						break;
 					}
@@ -364,6 +418,10 @@ int main(void) {
 						memset(buffer, 0, sizeof(buffer));
 						fputs(",", writeFile);
 						sprintf(buffer, "%f", zposArray[j + 1]);
+						fputs(buffer, writeFile);
+						memset(buffer, 0, sizeof(buffer));
+						fputs(",", writeFile);
+						sprintf(buffer, "%f", velocityArray[j + 1]);
 						fputs(buffer, writeFile);
 						memset(buffer, 0, sizeof(buffer));
 						fputs(",", writeFile);
@@ -406,6 +464,10 @@ int main(void) {
 						fputs(buffer, writeFile);
 						memset(buffer, 0, sizeof(buffer));
 						fputs(",", writeFile);
+						sprintf(buffer, "%f", velocityArray[j]);
+						fputs(buffer, writeFile);
+						memset(buffer, 0, sizeof(buffer));
+						fputs(",", writeFile);
 						trash = 0;
 						break;
 					}
@@ -423,6 +485,10 @@ int main(void) {
 							fputs(buffer, writeFile);
 							memset(buffer, 0, sizeof(buffer));
 							fputs(",", writeFile);
+							sprintf(buffer, "%f", velocityArray[j]);
+							fputs(buffer, writeFile);
+							memset(buffer, 0, sizeof(buffer));
+							fputs(",", writeFile);
 							trash = 0;
 							break;
 						}
@@ -436,6 +502,10 @@ int main(void) {
 							memset(buffer, 0, sizeof(buffer));
 							fputs(",", writeFile);
 							sprintf(buffer, "%f", zposArray[j + 1]);
+							fputs(buffer, writeFile);
+							memset(buffer, 0, sizeof(buffer));
+							fputs(",", writeFile);
+							sprintf(buffer, "%f", velocityArray[j + 1]);
 							fputs(buffer, writeFile);
 							memset(buffer, 0, sizeof(buffer));
 							fputs(",", writeFile);
@@ -477,6 +547,7 @@ int main(void) {
 	printf("쓴 파일을 닫았습니다.\n");
 
 	fclose(timeFile);     // 파일 포인터 닫기
+	fclose(velocityFile);     // 파일 포인터 닫기
 	fclose(xposFile);     // 파일 포인터 닫기
 	fclose(yposFile);     // 파일 포인터 닫기
 	fclose(zposFile);     // 파일 포인터 닫기
@@ -485,6 +556,7 @@ int main(void) {
 	free(bufferToCountCell);   // 동적 메모리 해제
 	free(bufferToPushData);   // 동적 메모리 해제
 	free(bufferToCountTime);   // 동적 메모리 해제
+	free(bufferToCountVelocity);   // 동적 메모리 해제
 	free(bufferToCountXpos);   // 동적 메모리 해제
 	free(bufferToCountYpos);   // 동적 메모리 해제
 	free(bufferToCountZpos);   // 동적 메모리 해제
